02d35da556276f7433fd9c1311a7f161ffda1aa8
Hasanuzzaman <hasanuzzamanbe@gmail.com>
2020-05-25 20:07:33 +0600

    Create index.vue

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h3>
+            {{msg}}
+        </h3>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            msg: "Hello from Nuxt"
+        };
+    }
+};
+</script>
